66b12c2a17cb810fe66d56e723923eee99e83840
Iwamgad <dagmawiharry2@gmail.com>
2022-11-17 12:43:15 +0000

    I edited task 3 again

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,11 @@
--- Create dev database and user
+-- This mysql script prepares a MySQL server for the project.
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+-- Create Database if doesn't Exist and set password for user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all Privileges to user on hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant select privileges to user on performa_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Flush privilages
+FLUSH PRIVILEGES;
